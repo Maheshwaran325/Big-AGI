bef64b80fc729a4390efe8633ae1cc28160b3128
Mahesh <maheshwaran325@gmail.com>
2024-02-12 16:11:05 +0000

    Sp V0.1

--- src/modules/llms/vendors/openrouter/OpenRouterSourceSetup.tsx
+++ src/modules/llms/vendors/openrouter/OpenRouterSourceSetup.tsx
@@ -48,9 +48,10 @@ export function OpenRouterSourceSetup(props: { sourceId: DModelSourceId }) {
     <Typography level='body-sm'>
       <Link href='https://openrouter.ai/keys' target='_blank'>OpenRouter</Link> is an independent service
       granting access to <Link href='https://openrouter.ai/docs#models' target='_blank'>exclusive models</Link> such
-      as GPT-4 32k, Claude, and more. <Link
+      as GPT-4 32k, Claude, and more. 
+      {/* <Link
       href='https://github.com/enricoros/big-agi/blob/main/docs/config-openrouter.md' target='_blank'>
-      Configuration &amp; documentation</Link>.
+      Configuration &amp; documentation</Link>. */}
     </Typography>
 
     <FormInputKey
